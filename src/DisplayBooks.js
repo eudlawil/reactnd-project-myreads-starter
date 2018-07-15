0d876cf63f677bb63d4a6371762a1d2cf312c147
Lisa Williams <lisa@underdogmedia.com>
2018-07-15 08:16:10 -0700

    feat: Breaking up the code into smaller components

---
+++ src/DisplayBooks.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
+import './App.css';
+
+class DisplayBooks extends Component {
+  state = {
+    //messages: [], //will hold this --> {usermame: 'Amy', text: 'a'}
+  };
+
+  render() {
+    const { allBooks, shelf } = this.props
+    let books = allBooks.filter(book => book.shelf === shelf.id )
+
+    return (
+
+      <div className="list-books-content">
+        <div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelf.name}</h2>
+
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+
+                {books.map((book, index) => (
+                  <li key={index} >
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={
+                            {
+                              width: 128,
+                              height: 193,
+                              backgroundImage: `url(${book.imageLinks.thumbnail})`
+                            }}>
+                        </div>
+                        <div className="book-shelf-changer">
+                          <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.author}</div>
+                    </div>
+                  </li>
+                ))}
+
+
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+DisplayBooks.propTypes = {
+ // messages: PropTypes.array.isRequired,
+ // user: PropTypes.object.isRequired,
+};
+
+export default DisplayBooks;
+
+//const { books, shelf } = props;
+//"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+//const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
---
+++ src/SearchPage.js
@@ -0,0 +1,52 @@
+
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
+import './App.css';
+
+class SearchPage extends Component {
+  state = {
+
+  };
+
+  render() {
+    //const { messages } = this.state;
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input type="text" placeholder="Search by title or author"/>
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
+
+
+    )
+  }
+}
+
+SearchPage.propTypes = {
+ // messages: PropTypes.array.isRequired,
+ // user: PropTypes.object.isRequired,
+};
+
+export default SearchPage;
+
+//const { books, shelf } = props;
+
+
+
+
